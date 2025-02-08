136ab92f6e5537d258f2d591daff463a7f78d79b
Soroush <d.soroush333@gmail.com>
2025-02-08 20:02:05 +0330

    fixed build error

--- All Scripts/Players/P1.cs
+++ All Scripts/Players/P1.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class P1 : MonoBehaviour
 {
